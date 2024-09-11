1dd72619cfa8f6e06ea1d0e5f078879fc109b0fa
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-09-11 00:19:46 -0600

    Add Comment table to DB

--- server/database/create_db.sql
+++ server/database/create_db.sql
@@ -4,6 +4,7 @@ USE SocialDB;
 
 DROP TABLE IF EXISTS user_data;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS post_comment;
 
 CREATE TABLE user_data(
     userID int NOT NULL AUTO_INCREMENT,
@@ -21,4 +22,15 @@ CREATE TABLE post(
     post_timestamp TIMESTAMP NOT NULL,
     PRIMARY KEY (postID),
     FOREIGN KEY (userID) REFERENCES user_data(userID)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE post_comment(
+    commentID INT NOT NULL AUTO_INCREMENT,
+    userID INT NOT NULL,
+    postID INT NOT NULL,
+    comment_content VARCHAR(128) NOT NULL,
+    comment_timestamp TIMESTAMP NOT NULL,
+    PRIMARY KEY (commentID),
+    FOREIGN KEY (userID) REFERENCES user_data(userID),
+    FOREIGN KEY (postID) REFERENCES post(postID)
 ) AUTO_INCREMENT=1;
