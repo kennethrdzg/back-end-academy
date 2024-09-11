fcc38f6a7d9978d8a99874ac30e5919f37a43a53
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-09-11 15:51:35 -0600

    Update User REST service to use JSON Web Tokens

--- server/src/main/java/com/kennethrdzg/socialapp/service/UserService.java
+++ server/src/main/java/com/kennethrdzg/socialapp/service/UserService.java
@@ -3,6 +3,6 @@ package com.kennethrdzg.socialapp.service;
 import com.kennethrdzg.socialapp.entities.User;
 
 public interface UserService {
-    public User register(User user);
-    public User login(User user);
+    public User register(User user) throws RuntimeException;
+    public User login(User user) throws RuntimeException;
 }
