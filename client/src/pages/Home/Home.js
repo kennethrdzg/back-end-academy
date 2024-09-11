98c35bcf41bd566eeff9bbcf66adfa6528e402f8
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-09-11 11:25:01 -0600

    Add Home page

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+class Home extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    async componentDidMount() {};
+
+    render(){
+        return(
+            <div>
+                <p>Home!</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
