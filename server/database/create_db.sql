ef5f29b1883952932085df97db50fd06bd7e7c21
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-09-10 23:11:32 -0600

    Add Post table to database

--- server/database/create_db.sql
+++ server/database/create_db.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS SocialDB;
 USE SocialDB;
 
 DROP TABLE IF EXISTS user_data;
+DROP TABLE IF EXISTS post;
 
 CREATE TABLE user_data(
     userID int NOT NULL AUTO_INCREMENT,
@@ -11,4 +12,13 @@ CREATE TABLE user_data(
     # salt VARCHAR(12) NOT NULL,
     # hashedPassword VARCHAR(64) NOT NULL,
     PRIMARY KEY (userID), UNIQUE (username)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE post(
+    postID INT NOT NULL AUTO_INCREMENT,
+    userID INT NOT NULL,
+    post_content VARCHAR(128) NOT NULL,
+    post_timestamp TIMESTAMP NOT NULL,
+    PRIMARY KEY (postID),
+    FOREIGN KEY (userID) REFERENCES user_data(userID)
 ) AUTO_INCREMENT=1;
