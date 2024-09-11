76104871103a59c51cf0980bfcbd18a6d50514b1
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-09-11 02:31:58 -0600

    Remove Comment REST service delete endpoint

--- server/src/main/java/com/kennethrdzg/socialapp/rest/CommentRestController.java
+++ server/src/main/java/com/kennethrdzg/socialapp/rest/CommentRestController.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,8 +38,8 @@ public class CommentRestController{
         // return null;
     }
 
-    @DeleteMapping("/{commentId}")
-    public void deleteComment(@RequestBody int commentId){
-        this.commentService.deleteComment(commentId);
-    }
+    // @DeleteMapping("/{commentId}")
+    // public void deleteComment(@RequestBody int commentId){
+    //     this.commentService.deleteComment(commentId);
+    // }
 }
