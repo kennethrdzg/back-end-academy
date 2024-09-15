5d7438c2af30de631947071d95c7baeef3e6c12d
Kenneth Rodriguez <kenneth.rodriguez.developer@gmail.com>
2024-09-15 15:04:36 -0600

    Add Notification Service

---
+++ smalltalk-server/src/main/java/com/kennethrdzg/smalltalk/service/NotificationService.java
@@ -0,0 +1,21 @@
+package com.kennethrdzg.smalltalk.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService{
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    
+    private RabbitTemplate rabbitTemplate;
+
+    public NotificationService(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendNotification(String routingKey, String message){
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
